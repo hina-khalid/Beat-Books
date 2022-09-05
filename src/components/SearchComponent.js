392105adbaaaaebad18a073586ccf5dd252e4429
Hina Khalid <k.hinaa87@gmail.com>
2022-09-04 23:49:58 -0400

    validating user input

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import styles from "../stylesheet/styles.css";
+import "../stylesheet/styles.css";
 import logo from "../../assets/logo.png"
 
 function SearchComponent() {
@@ -16,6 +16,14 @@ function SearchComponent() {
         setAuthor(e.target.value);
     };
 
+    const handleSubmit = () => {
+        if (!bookName || !author) {
+            alert("Please enter both fields.");
+            return;
+        }
+        navigate("/loading");
+    }
+
     return (
         <div className="outsideOfForm">
             <div className="form">
@@ -28,59 +36,10 @@ function SearchComponent() {
                 <b><label className="label"> Author </label></b>
                 <input type='text' value={author} onChange={handleAuthor} className="userInput" />
                 <br />
-                <button onClick={() => navigate("/loading")} className="submitButton">Submit</button>
+                <button onClick={handleSubmit} className="submitButton">Submit</button>
             </div >
         </div >
     )
 }
-
-
-// class SearchComponent extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             bookName: "",
-//             author: "",
-//         }
-//         // const navigate = useNavigate();
-
-//         this.handleChange = this.handleChange.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//         // this.navigateToLoading = this.navigateToLoading.bind(this);
-//     }
-
-//     handleChange({ target }) {
-//         this.setState({ [target.name]: target.value })
-//     };
-
-//     handleSubmit(e) {
-//         e.preventDefault();
-//         console.log('checking to see if the handle submit func is invoked')
-//     }
-
-//     // once the user hits the submit button, we want to navigate them to the loading page
-//     navigateToLoading() {
-//         console.log('inside loading')
-//         navigate('/LoadingComponent', { replace: true });
-//     }
-
-//     render() {
-//         return (
-//             <React.Fragment>
-//                 <form>
-//                     <div>
-//                         <b><label> Book Name: </label></b>
-//                         <input type="text" name="bookName" value={this.state.bookName} onChange={this.handleChange} />
-//                     </div>
-//                     <div>
-//                         <b><label> Author: </label></b>
-//                         <input type="text" name="author" value={this.state.author} onChange={this.handleChange} />
-//                     </div>
-//                     <SubmitButton onClick={this.handleSubmit} />
-//                 </form>
-//             </React.Fragment>
-//         )
-//     }
-// }
 
 export default SearchComponent;
